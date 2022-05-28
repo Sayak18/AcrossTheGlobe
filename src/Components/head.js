dee4512b7ce013df9cdc366df3b3d730280e2653
Sayak Bhattacharjee <sayak718.s@gmail.com>
2022-05-28 22:40:39 +0530

    design updated

--- src/Components/head.js
+++ src/Components/head.js
@@ -8,51 +8,6 @@ export default function HeadBar(props) {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  // return (
-  //   <div className="head">
-  //     <Modal show={show} onHide={handleClose}>
-  //       <Modal.Header closeButton>
-  //         <Modal.Title>Create Account</Modal.Title>
-  //       </Modal.Header>
-  //       <Modal.Body>
-  //         <form>
-  //           <input type="text" placeholder="First Name" name="firstName" />
-  //           <input type="text" placeholder="Last Name" name="lastName" />
-  //           <input type="email" placeholder="Email" name="email" />
-  //         </form>
-  //       </Modal.Body>
-  //       <Modal.Footer>
-  //         <Button variant="secondary" onClick={handleClose}>
-  //           Close
-  //         </Button>
-  //         <Button variant="primary" onClick={handleClose}>
-  //           Create account
-  //         </Button>
-  //       </Modal.Footer>
-  //     </Modal>
-  //     <div className="logo">
-  //       <span className="green">ATG.</span>
-  //       <span className="black">
-  //         W<img className="head-logo" src={logo} />
-  //         RLD
-  //       </span>
-  //     </div>
-  //     <div className="search">
-  //       <input
-  //         type="search"
-  //         placeholder="Search for your favorite groups in ATG"
-  //       />
-  //     </div>
-  //     <div className="right">
-  //       <span>Create account.</span>
-  //       <span className="blue">It's free!</span>
-  //       <button className="head-btn" onClick={handleShow}>
-  //         <img src={btn} />
-  //       </button>
-  //     </div>
-  //   </div>
-  // );
-
   return (
     <Navbar bg="light" expand="lg">
       <Modal show={show} onHide={handleClose}>
@@ -83,16 +38,16 @@ export default function HeadBar(props) {
             RLD
           </span>
         </Navbar.Brand>
-        {/* <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
             style={{ maxHeight: "100px" }}
             navbarScroll
-          > */}
-        {/* <Nav.Link href="#action1">Home</Nav.Link>
+          >
+            {/* <Nav.Link href="#action1">Home</Nav.Link>
         <Nav.Link href="#action2">Link</Nav.Link> */}
-        {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
+            {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
           <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
           <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
           <NavDropdown.Divider />
@@ -100,24 +55,24 @@ export default function HeadBar(props) {
             Something else here
           </NavDropdown.Item>
         </NavDropdown> */}
-        {/* <Nav.Link href="#" disabled>
+            {/* <Nav.Link href="#" disabled>
           Link
         </Nav.Link> */}
-        {/* </Nav> */}
-        <Form className="d-flex">
-          <FormControl
-            type="search"
-            placeholder="Search for your favorite groups in ATG"
-            className="me-2"
-            aria-label="Search"
-          />
-          <Button variant="outline-success">Search</Button>
-        </Form>
-        <button className="head-btn" onClick={handleShow}>
-          <img src={btn} />
-          Create account.It's free!
-        </button>
-        {/* </Navbar.Collapse> */}
+          </Nav>
+          <Form className="d-flex">
+            <FormControl
+              type="search"
+              placeholder="Search for your favorite groups in ATG"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+          <button className="head-btn" onClick={handleShow}>
+            <img src={btn} />
+            Create account.It's free!
+          </button>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
